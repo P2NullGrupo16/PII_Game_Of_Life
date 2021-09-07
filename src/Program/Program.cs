53431de925015351c9f6e9f407291ea455dec052
jumpert <juan.perezc@correo.ucu.edu.uy>
2021-09-07 01:06:06 -0300

    JPerez modelo v.01

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 
+
 namespace PII_Game_Of_Life
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LogicaJuego juego1 = new LogicaJuego();
+            juego1.GenerarTablero();
+            ImprimirTablero a = new ImprimirTablero();
+            a.Imprimir(juego1.Juego());
+            
+            
         }
     }
 }
